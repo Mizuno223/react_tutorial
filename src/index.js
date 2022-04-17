66bdb7cb0647d57eaeb74c36808f257bd3788278
mizuno <31834@toyota.kosen-ac.jp>
2022-04-18 01:20:32 +0900

    class Square=>function Square

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square" onClick={function() {console.log('click'); }}> 
-                {this.props.value}
-            </button>
-        );
-    }
-}
+const Square = ({ value }) => {
+    return <button className="square" onClick={function () { console.log('click'); }}>{value}</button>;
+};
 
 class Board extends React.Component {
     renderSquare(i) {
