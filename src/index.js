a3d0388717a1e5e5ede3c2e8dec9ae4fbe2f63f7
mizuno <31834@toyota.kosen-ac.jp>
2022-04-18 01:38:36 +0900

    don't use "ReactDOM.render()"

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 
 const Square = ({ value }) => {
@@ -51,7 +51,6 @@ const Game = () => {
 
 // ========================================
 
-ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<Game />);
