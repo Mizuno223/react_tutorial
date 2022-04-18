fd9892dd574661f9ddd90a154b76703afc659eb7
mizuno <31834@toyota.kosen-ac.jp>
2022-04-18 23:25:25 +0900

    Save history

--- src/index.js
+++ src/index.js
@@ -7,66 +7,14 @@ const Square = ({ value, onClick }) => {
     return <button className="square" onClick={() => onClick()}>{value}</button>;
 };
 
-const Board = () => {
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [xIsNext, setXIsNext] = useState(true);
-
-    const handleClick = (i) => {
-        const _squares = squares.slice();
-
-        if (calculateWinner(_squares) || _squares[i]) {
-            return;
-        }
-        _squares[i] = xIsNext ? 'X' : 'O';
-        setSquares(_squares);
-        setXIsNext(!xIsNext);
-    };
-
-    const calculateWinner = (squares) => {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }
-        }
-        // lines.some((line) => {
-        //     const [a, b, c] = line;
-        //     console.log([a, b, c]);
-        //     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        //         console.log(squares[a]);
-        //         return squares[a];
-        //     } else {
-        //         return null;
-        //     }
-        // });
-        return null;
-    };
-
+const Board = ({ squares, onClick }) => {
     const renderSquare = (i) => {
-        return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+        return <Square value={squares[i]} onClick={() => onClick(i)} />;
     };
-
-    const winner = calculateWinner(squares);
-    let status;
-    if (winner) {
-        status = 'Winner: ' + winner;
-    } else {
-        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-    }
 
     return (
         <div>
-            <div className="status">{status}</div>
+            <div className="status"></div>
             <div className="board-row">
                 {renderSquare(0)}
                 {renderSquare(1)}
@@ -87,13 +35,58 @@ const Board = () => {
 };
 
 const Game = () => {
+    const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+    const [xIsNext, setXIsNext] = useState(true);
+    const handleClick = (i) => {
+        const _history = history;
+        const current = _history[_history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return ;
+        }
+        squares[i] = xIsNext ? 'X' : 'O';
+
+        setHistory(_history.concat([{ squares: squares }]));
+        setXIsNext(!xIsNext);
+    };
+
+    const calculateWinner = (squares) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    };
+
+    const _history = history;
+    const current = _history[_history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
     return (
         <div className="game">
             <div className="game-board">
-                <Board />
+                <Board squares={current.squares} onClick={(i) => handleClick(i)} />
             </div>
             <div className="game-info">
-                <div>{/* status */}</div>
+                <div>{ status }</div>
                 <ol>{/* TODO */}</ol>
             </div>
         </div>
